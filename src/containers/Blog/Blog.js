f67c52f8651003ce9bdf96a905c37da369b2a16a
John Huynh <jhnhnyh9@gmail.com>
2018-09-25 21:53:55 -0700

    implement routes and links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -12,14 +13,23 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'}}>
+                                New Post</Link>
+                            </li>
                         </ul>
                     </nav>
                 </header>
-                {/* <Route path='/' exact render={<Posts /> } /> */}
-                <Route path='/' exact component={Posts} />
 
+                <Route path='/' exact component={Posts} />
+                <Route path='/new-post' component={NewPost} />
+                
                 {/* <section>
                     <FullPost id={this.state.selectedPostID}/>
                 </section>
