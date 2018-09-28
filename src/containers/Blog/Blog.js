ea5a4603cdc51c3a3e2b4d8a5fbc0a070f8820e6
John Huynh <jhnhnyh9@gmail.com>
2018-09-27 17:58:41 -0700

    imeplmented route switches and query params

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
 
@@ -33,9 +34,12 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-
-                <Route path='/' exact component={Posts} />
-                <Route path='/new-post' component={NewPost} />
+                
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
                 
                 {/* <section>
                     <FullPost id={this.state.selectedPostID}/>
